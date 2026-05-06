f7ed4fbf3fa6e862af0436edc94d5ad696bba0e9
Sergio Camilo Espinosa Botero <sespinosab@unal.edu.co>
2026-05-05 22:56:19 -0500

    Se añadieron líneas al archivo de texto y el codigo necesario para leerlo

--- labgit.cpp
+++ labgit.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<fstream>
+#include<sstream>
 
 using namespace std;
 int main (){
+	ifstream archivoEntrada("input/archivoEntrada.txt");
 	
+	if (nombre_archivo.is_open()) {
+	  cout<<"Archivo abierto"<<endl;
+	  string linea;
+	
+	  while (getline(nombre_archivo, linea)) {
+	    
+	  }
+	}
 }
